8eeea31cf0406250812e18e2dadae28876401f5b
vladotoka <vladimir.k.terziev@tutanota.com>
2022-06-04 19:52:38 +0300

    initial loading request

--- src/screens/SearchSreen.js
+++ src/screens/SearchSreen.js
@@ -1,31 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+
 const SearchSreen = () => {
 
+  const onTermChange = (newTerm) => setTerm(newTerm);
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrormessage] = useState('');
 
-  const onTermChange = (newTerm) => setTerm(newTerm);
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'm259ne'
-      }
-    });
-    setResults(response.data.businesses);
-    console.log(response.data.businesses);
-  }
+  const searchApi = async (searchTerm) => {
+    try {
+      setErrormessage('');
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'm259ne'
+        }
+      });
+      setResults(response.data.businesses);
+      console.log(response.data.businesses);
+    } catch (err) {
+      setErrormessage(`Something went wrong. Error message: ${err}`);
+    }
+  };
+
+  //Default search on firstmount  
+  useEffect(() => {
+    searchApi('Milano');
+  }, []);
 
   return (
     <View style={styles.page}>
-      <SearchBar term={term} onTermChange={onTermChange} onTermSubmit={searchApi} />
+      <SearchBar term={term} onTermChange={onTermChange} onTermSubmit={() => searchApi(term)} />
       <Text>SearchSreen</Text>
-      <Text>We have found {results.length} results</Text>
+      {!errorMessage ? <Text> We have found {results.length} results </Text> : <Text>{errorMessage}</Text>}
     </View>
   )
 }
